977b25f6d0ba4e48317e28df22a4c8522805e414
Emiliano López <emilianolch@gmail.com>
2022-07-03 20:09:28 -0300

    listen for notifications

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -16,9 +16,13 @@ class PaymentsController < ApplicationController
         }
       ],
       back_urls: {
-        success: payments_success_url
-      }
+        success: payments_success_url,
+        failure: payments_failure_url,
+        pending: payments_pending_url
+      },
+      notification_url: ENV['NGROK'] ? payments_notification_url(protocol: 'https', host: ENV['NGROK'], port: nil) : payments_notification_url
     }
+
     preference_response = sdk.preference.create(preference_data)
     @preference = preference_response[:response]
 
@@ -27,4 +31,14 @@ class PaymentsController < ApplicationController
   end
 
   def success; end
+
+  def failure; end
+
+  def pending; end
+
+  def notification
+    respond_to do |format|
+      format.json { head :no_content }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'payments/new'
   get 'payments/success'
+  get 'payments/failure'
+  get 'payments/pending'
+  post 'payments/notification'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
